f9e9f82ccf47a0b17c49d7f75a1623afc95c3949
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-27 03:24:09 +0600

    Logo & Background Color Implemented On Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
 
+const logo = (
+	<div className={styles.logo}>
+		<Link to={"/"}>
+			<h2>
+				Shop<span>Ito</span>
+			</h2>
+		</Link>
+	</div>
+);
+
 const Header = () => {
 	return (
 		<header>
-			<div className={styles.header}>
-				<div className={styles.logo}>
-					<Link to={"/"}>
-						<h2>
-							Shop<span>Ito</span>
-						</h2>
-					</Link>
-				</div>
-			</div>
+			<div className={styles.header}>{logo}</div>
 		</header>
 	);
 };
