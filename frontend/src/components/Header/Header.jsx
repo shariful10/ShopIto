8095d6d725667097ccd3b3b8e89bf7b36c88ad66
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-27 03:09:03 +0600

    SCSS Implemented

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+	return (
+		<header>
+			<div className={styles.header}>
+				<div className={styles.logo}>
+					<Link to={"/"}>
+						<h2>
+							Shop<span>Ito</span>
+						</h2>
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
