55ef50d33188bf9661d5c08fc4a061cee4538a33
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-27 04:02:46 +0600

    Nav Items Implemented On Navbar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Header.module.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-const logo = (
+export const logo = (
 	<div className={styles.logo}>
 		<Link to={"/"}>
 			<h2>
@@ -12,10 +12,34 @@ const logo = (
 	</div>
 );
 
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
+
 const Header = () => {
 	return (
 		<header>
-			<div className={styles.header}>{logo}</div>
+			<div className={styles.header}>
+				{logo}
+				<nav>
+					<ul>
+						<li>
+							<NavLink to="/shop" className={activeLink}>Shop</NavLink>
+						</li>
+					</ul>
+				</nav>
+				<div className={styles["header-right"]}>
+					<span className={styles.links}>
+						<NavLink to={"/login"} className={activeLink}>
+							Login
+						</NavLink>
+						<NavLink to={"/register"} className={activeLink}>
+							Register
+						</NavLink>
+						<NavLink to={"/order-history"} className={activeLink}>
+							MyOrder
+						</NavLink>
+					</span>
+				</div>
+			</div>
 		</header>
 	);
 };
