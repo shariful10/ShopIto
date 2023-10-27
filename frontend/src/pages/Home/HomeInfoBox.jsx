16ee8d420d8833a19186b1aac556e68a7f9d1c1c
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-28 01:19:23 +0600

    HomeInfoBox Json Data Implemented

--- frontend/src/pages/Home/HomeInfoBox.jsx
+++ frontend/src/pages/Home/HomeInfoBox.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import {
+	BsCartCheck,
+	BsClockHistory,
+	BsFillCreditCardFill,
+} from "react-icons/bs";
+import { FaShippingFast } from "react-icons/fa";
+
+const data = [
+	{
+		icon: <FaShippingFast size={30} color="#8cb4f5" />,
+		heading: "Free Shipping",
+		text: "We offer free shipping on special products",
+	},
+	{
+		icon: <BsFillCreditCardFill size={30} color="#f7d272" />,
+		heading: "Secure Payment",
+		text: "Make secure payment for your product.",
+	},
+	{
+		icon: <BsCartCheck size={30} color="#fa82ea" />,
+		heading: "Quality Products",
+		text: "We sell products from only tested and proven brands.",
+	},
+	{
+		icon: <BsClockHistory size={30} color="#82fa9e" />,
+		heading: "24/7 Support",
+		text: "Get access to support from our exprt support team.",
+	},
+];
 
 const HomeInfoBox = () => {
-  return (
-    <div>HomeInfoBox</div>
-  )
-}
+	return <div>HomeInfoBox</div>;
+};
 
-export default HomeInfoBox
+export default HomeInfoBox;
