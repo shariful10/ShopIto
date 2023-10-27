89dc1d631a3d3fe80e3705dfb9a5f2e80de70805
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-27 15:19:14 +0600

    Header Modify

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link, NavLink } from "react-router-dom";
@@ -16,12 +16,22 @@ export const logo = (
 const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
 const Header = () => {
+	const [showMenu, setShowMenu] = useState(false);
+
+	const toggleMenu = () => {
+		setShowMenu(!showMenu);
+	};
+
+	const hideMenu = () => {
+		setShowMenu(false);
+	};
+
 	const cart = (
 		<span className={styles.cart}>
 			<Link to={"/cart"}>
 				Cart
 				<FaShoppingCart size={20} />
-            <p>0</p>
+				<p>0</p>
 			</Link>
 		</span>
 	);
@@ -38,21 +48,24 @@ const Header = () => {
 							</NavLink>
 						</li>
 					</ul>
+					<div className={styles["header-right"]}>
+						<span className={styles.links}>
+							<NavLink to={"/login"} className={activeLink}>
+								Login
+							</NavLink>
+							<NavLink to={"/register"} className={activeLink}>
+								Register
+							</NavLink>
+							<NavLink to={"/order-history"} className={`${activeLink}`}>
+								My Order
+							</NavLink>
+						</span>
+						{cart}
+					</div>
 				</nav>
-				<div className={styles["header-right"]}>
-					<span className={styles.links}>
-						<NavLink to={"/login"} className={activeLink}>
-							Login
-						</NavLink>
-						<NavLink to={"/register"} className={activeLink}>
-							Register
-						</NavLink>
-						<NavLink to={"/order-history"} className={`${activeLink} flex`}>
-							My Order
-						</NavLink>
-					</span>
-					{cart}
-				</div>
+            <div className={styles["menu-icon"]}>
+               {cart}
+            </div>
 			</div>
 		</header>
 	);
