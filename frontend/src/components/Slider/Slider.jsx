06716e6c1eec9c2d5c3466f89ddb36f08974a470
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-27 23:47:46 +0600

    Image Implemented On Slider

--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Slider.scss";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { sliderData } from "./slider-data";
 
 const Slider = () => {
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	const prevSlide = () => {};
+	const nextSlide = () => {};
+
 	return (
 		<div className="slider">
 			<AiOutlineArrowLeft onClick={prevSlide} className="arrow prev" />
 			<AiOutlineArrowRight onClick={nextSlide} className="arrow next" />
+			{sliderData.map((slide, i) => {
+				const { image, heading, desc } = slide;
+				return (
+					<div
+						key={i}
+						className={i === currentSlide ? "slide current" : "slide"}
+					>
+						{i === currentSlide && (
+							<>
+								<img src={image} alt="Slide" />
+							</>
+						)}
+					</div>
+				);
+			})}
 		</div>
 	);
 };
