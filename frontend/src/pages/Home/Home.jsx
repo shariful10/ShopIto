a4e7c7c82303bded4cdeb6d90c7c6c4a652ff4de
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-28 01:00:43 +0600

    Home.scss File Implemented

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Slider from "../../components/Slider/Slider";
+import HomeInfoBox from "./HomeInfoBox";
 
 const Home = () => {
 	return (
 		<>
 			<Slider />
+			<HomeInfoBox />
 		</>
 	);
 };
---
+++ frontend/src/pages/Home/HomeInfoBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeInfoBox = () => {
+  return (
+    <div>HomeInfoBox</div>
+  )
+}
+
+export default HomeInfoBox
