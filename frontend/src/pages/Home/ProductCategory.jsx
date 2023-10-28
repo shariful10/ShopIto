06b80dccab1331afed61b102e3691745eb0b56c5
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-28 20:44:38 +0600

    Cotegories Data Implemented

--- frontend/src/pages/Home/ProductCategory.jsx
+++ frontend/src/pages/Home/ProductCategory.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
+const categories = [
+	{
+		id: 1,
+		title: "Gadgets",
+		image: "https://i.ibb.co/5GVkd3m/c1.jpg",
+	},
+	{
+		id: 2,
+		title: "Womens Fashion",
+		image: "https://i.ibb.co/nQKLjrW/c2.jpg",
+	},
+	{
+		id: 3,
+		title: "Sport Sneakers",
+		image: "https://i.ibb.co/fNkBYgr/c3.jpg",
+	},
+];
+
 const ProductCategory = () => {
 	return <div>ProductCategory</div>;
 };
