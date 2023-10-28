b022210e7f470c5b3e621f76a57931e472a5cf6a
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-28 21:05:43 +0600

    ProducrDetails.module.scss File Implemented

--- frontend/src/pages/Home/ProductCategory.jsx
+++ frontend/src/pages/Home/ProductCategory.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import "./ProductCategory.scss";
+import H3 from "./../../components/H3/H3";
+import { useNavigate } from "react-router-dom";
+import Buttons from "./../../components/Buttons/Buttons";
 
 const categories = [
 	{
@@ -18,8 +22,31 @@ const categories = [
 	},
 ];
 
+const Category = ({ title, image }) => {
+	const navigate = useNavigate();
+	return (
+		<div className="category">
+			<H3>{title}</H3>
+			<img src={image} alt="img" />
+			<Buttons className="bg-grey rounded-[6px] mt-5" onClick={() => navigate("/shop")}>
+				{"Shop Now >>>"}
+			</Buttons>
+		</div>
+	);
+};
+
 const ProductCategory = () => {
-	return <div>ProductCategory</div>;
+	return (
+		<div className="categories">
+			{categories.map((cat) => {
+				return (
+					<div key={cat.id} className="flex justify-center items-center">
+						<Category title={cat.title} image={cat.image} />
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default ProductCategory;
